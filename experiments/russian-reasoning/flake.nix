79af1ab9911dea77cd8db8587bf436ae657e403a
DenisSud <sudakov.denis.2007@gmail.com>
2025-02-20 17:19:14 +0300

    added submodule

---
+++ experiments/russian-reasoning/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [ python3 ] ++
+            (with pkgs.python3Packages; [
+              pip
+              torch-bin
+              tqdm
+              transformers
+              datasets
+              venvShellHook
+            ]);
+        };
+      });
+    };
+}
